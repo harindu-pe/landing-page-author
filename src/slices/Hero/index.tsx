8a37fe2a4d838267580b1ed06f4fe88729903126
Harindu Egalla <harindu.e@asyncmetric.com>
2024-04-21 01:22:35 +0530

    hero animations done

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -1,5 +1,11 @@
-import { Content } from "@prismicio/client";
+"use client";
+
+import { Content, KeyTextField } from "@prismicio/client";
 import { SliceComponentProps } from "@prismicio/react";
+import { useRef } from "react";
+
+import gsap, { random } from "gsap"; // <-- import GSAP
+import { useGSAP } from "@gsap/react"; // <-- import the hook from our React package
 
 /**
  * Props for `Hero`.
@@ -10,14 +16,84 @@ export type HeroProps = SliceComponentProps<Content.HeroSlice>;
  * Component for "Hero" Slices.
  */
 const Hero = ({ slice }: HeroProps): JSX.Element => {
+  gsap.registerPlugin(useGSAP);
+  const container = useRef(null);
+
+  useGSAP(
+    () => {
+      // gsap code here...
+      const tl = gsap.timeline();
+      tl.fromTo(
+        ".name-animation",
+        { x: -100, opacity: 0, rotate: -10 },
+        {
+          x: 0,
+          opacity: 1,
+          rotate: 0,
+          ease: "elastic.out(1,0.3)",
+          duration: 1,
+          transformOrigin: "left top",
+          stagger: {
+            each: 0.1,
+            from: "random",
+          },
+        }
+      );
+      tl.fromTo(
+        ".job-title",
+        {
+          y: 20,
+          opacity: 0,
+          scale: 1.2,
+        },
+        {
+          y: 0,
+          opacity: 1,
+          scale: 1,
+          duration: 1,
+          ease: "elastic.out(1,0.3)",
+        }
+      );
+    },
+    { scope: container }
+  );
+
+  const renderLetters = (name: KeyTextField, key: string) => {
+    if (!name) return;
+    return name.split("").map((letter, index) => (
+      <span
+        key={index}
+        className={`name-animation name-animation-${key} inline-block opacity-0`}
+      >
+        {letter}
+      </span>
+    ));
+  };
+
   return (
     <section
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
+      ref={container}
     >
-      <span className="text-red-500">{slice.primary.first_name}</span>
-      <>{slice.primary.last_name}</>
-      <>{slice.primary.tag_line}</>
+      <div className="grid min-h-[70vh] grid-cols-1 md:grid-cols-2 items-center">
+        <div className="col-start-1 md:row-start-1 border">
+          <h1
+            className="mb-8 text-[clamp(3rem,20vmin,20rem)] md:text-[9rem] lg:text-[clamp(3rem,20vmin,20rem)] font-extrabold leading-[1.05] tracking-tighter"
+            aria-label={`${slice.primary.first_name} ${slice.primary.last_name}`}
+          >
+            <span className="block text-slate-300">
+              {renderLetters(slice.primary.first_name, "first")}
+            </span>
+            <span className="block text-slate-500">
+              {renderLetters(slice.primary.last_name, "last")}
+            </span>
+          </h1>
+          <span className="job-title block bg-gradient-to-tr from-yellow-500 via-yellow-200 to-yellow-500 bg-clip-text text-2xl font-bold uppercase tracking-[0.2em] text-transparent opacity-0 md:text-4xl">
+            {slice.primary.tag_line}
+          </span>
+        </div>
+      </div>
     </section>
   );
 };
